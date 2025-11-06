85155df07e67fcdafacf1c8ad9e12023a84aec9c
LeoRoccoBreedt <leobreedt@gmail.com>
2025-11-06 20:55:47 +0100

    feat: add streamlit app with LanfChain again connected to a send_email tool which also acts as QA bot.

---
+++ st_app.py
@@ -0,0 +1,59 @@
+import streamlit as st
+
+# LangChain code
+from langchain.agents import create_agent
+from langchain_core.tools import tool
+from langchain.agents.middleware import HumanInTheLoopMiddleware
+
+from langgraph.checkpoint.memory import InMemorySaver 
+
+from dotenv import load_dotenv
+load_dotenv()
+
+@tool()
+def send_email(recipient: str, subject: str, body: str) -> str:
+    """Send an email to a recipient.
+    Args:
+        recipient: Email address of the recipient.
+        subject: Subject line of the email.
+        body: Body content of the email.
+    Returns:
+        Confirmation message.
+    """
+    return f"Email sent successfully to {recipient}"
+
+# Sidebar # TODO: Add ability to set OpenAI API key
+# with st.sidebar:
+    # openai_api_key = st.text_input("OpenAI API key", type="password")
+
+agent = create_agent(
+    model="openai:gpt-4o",
+    tools=[send_email],
+    system_prompt="You are a helpful email assistant",
+    #middleware=[HumanInTheLoopMiddleware(interrupt_on={"send_email": True})],
+    #checkpointer=InMemorySaver(), # In production, use a persistent checkpointer like AsyncPostgresSaver.
+)
+
+st.title("🦜🔗 Email Assistant")
+
+# Create session state parameters
+st.session_state.history = st.session_state.get("history", [])
+st.session_state.stage = st.session_state.get("stage", "input")
+st.session_state.agent_responses = st.session_state.get("agent_responses", [])
+
+
+for message in st.session_state.history:
+    with st.chat_message(message["role"]):
+        st.write(message["content"])
+
+if user_input := st.chat_input("Ask me to send an email"):
+    st.session_state.history.append({"role": "user", "content": user_input})
+
+    agent_response = agent.invoke(
+        {"messages": [{"role": "user", "content": user_input}]},
+    )
+
+    agent_response_message = agent_response["messages"][-1].content
+    st.session_state.history.append({"role": "assistant", "content": agent_response_message})
+    st.session_state.agent_responses = agent_response
+    st.rerun()
